821b08be85cd76201e7123abbeae1dd45e4bb536
sarfraz <sarfraziub123@gmail.com>
2018-01-24 13:04:21 +0500

    Server side error display on top of the item page

--- Code/WebApp/src/app/entities/measurement.ts
+++ Code/WebApp/src/app/entities/measurement.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Measurement {
-    id:number = 0;
-    code:string=null;
-    description:string=null;
+    //id:number = 0;
+    //code:string=null;
+    //description:string=null;
 }
